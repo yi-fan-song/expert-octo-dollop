c404b2507e628aa655fae16a02b7a4038c725329
yi fan song <yfsong00@gmail.com>
2020-11-25 17:35:29 -0500

    turtle placing

---
+++ Mining Turtle Scripts/placeLine.lua
@@ -0,0 +1,20 @@
+local fuelLevel = turtle.getFuelLevel()
+
+if not (fuelLevel == "unlimited") and (fuelLevel > 128) then
+	local item = turtle.getItemDetail()
+	if item.name == "minecraft:coal" then
+		turtle.refuel(item.count - 1)
+	end
+	if turtle.getFuelLevel() < 128 then
+		print("not enough fuel")
+		do return end
+	end
+end
+
+turtle.select(2)
+
+while(turtle.back()) do
+	turtle.place()
+end
+
+turtle.select(1)
